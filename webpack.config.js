6916cc5e2f9d5a83bdf20d029cb2de5b890422c9
Robert Wise <robcodehub@gmail.com>
2019-09-29 02:14:59 +0700

    create package.json and webpack config files

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: './src',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
